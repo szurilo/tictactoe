e012864af5426c5145862154a35f1b48fe8afb04
József Tar <jozsef.tar@atollgroup.eu>
2024-09-26 15:55:36 +0200

    add basic game mechanism

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+function Field({ currentValue, onFieldClick }) {
+  return (
+    <button className='border' onClick={onFieldClick}>{currentValue}</button>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [fields, setFields] = useState(['', '', '', '', '', '', '', '', '']);
+  const [turn, setTurn] = useState(false);
+
+  const handleClick = (index) => {
+    if (turn) {
+      fields[index] = 'x';
+    } else {
+      fields[index] = '0';
+    }
+    setTurn(!turn);
+  }
+
+  const restart = () => {
+    setFields(['', '', '', '', '', '', '', '', '']);
+    setTurn(false);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ display: 'flex', marginBottom: '10px' }}>The next player is: {turn ? 'x' : 'o'}</div >
+      <div className='grid'>
+        <Field currentValue={fields[0]} onFieldClick={() => handleClick(0)}></Field>
+        <Field currentValue={fields[1]} onFieldClick={() => handleClick(1)}></Field>
+        <Field currentValue={fields[2]} onFieldClick={() => handleClick(2)}></Field>
+        <Field currentValue={fields[3]} onFieldClick={() => handleClick(3)}></Field>
+        <Field currentValue={fields[4]} onFieldClick={() => handleClick(4)}></Field>
+        <Field currentValue={fields[5]} onFieldClick={() => handleClick(5)}></Field>
+        <Field currentValue={fields[6]} onFieldClick={() => handleClick(6)}></Field>
+        <Field currentValue={fields[7]} onFieldClick={() => handleClick(7)}></Field>
+        <Field currentValue={fields[8]} onFieldClick={() => handleClick(8)}></Field>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button style={{ display: 'flex', marginTop: '10px' }} onClick={restart}>Restart</button>
     </>
   )
 }
